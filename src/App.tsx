87232fcf40dea9b2aae356c0e65bae197c2522bd
mutafow <mutafow@gmail.com>
2020-10-15 16:44:40 +0300

    Setup eslint  + prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>Boilerplate code for EdTech front-end</p>
-            </header>
-        </div>
-    );
-}
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Boilerplate code for EdTech front-end</p>
+      </header>
+    </div>
+  );
+};
 
 export default App;
