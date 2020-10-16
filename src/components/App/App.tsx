ca9ea398adf6ff3d8b7ccea12fb1c2469edc608d
Boris Mutafov <bmutafov@gmail.com>
2020-10-16 12:27:23 +0300

    Boilerplate (#9)
    * create-react-app with typescript
    
    * Remove unneccesary stuff
    
    * Setup eslint  + prettier
    
    * lint, deploy and redeploy
    
    * Restructure
    
    * fix eslint
    
    * eslint 2

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Boilerplate code for EdTech front-end</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
