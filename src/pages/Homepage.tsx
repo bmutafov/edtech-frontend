e074220ce98365762b940445139f4591cc38bb52
Boris Mutafov <bmutafov@gmail.com>
2020-11-07 12:25:39 +0200

    Implement routing (#25)

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Homepage: React.FC = () => {
+  return (
+    <div>
+      <header>
+        <p>Boilerplate code for EdTech front-end</p>
+      </header>
+    </div>
+  );
+};
+
+export default Homepage;
