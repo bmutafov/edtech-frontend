ff546d05ae3c7808123628adcc5715675e4dcffb
julioandre <julioandre275@gmail.com>
2020-12-02 20:54:37 +0100

    Bug Fixes

--- src/components/SideBar/SideBar.styles.ts
+++ src/components/SideBar/SideBar.styles.ts
@@ -1,7 +1,5 @@
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 
-const drawerWidth = 240;
-
 export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     // necessary for content to be below app bar
