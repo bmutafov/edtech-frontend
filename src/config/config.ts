f66d05867f9c2633438d6f98e93763d63cdacf8c
mutafow <mutafow@gmail.com>
2021-01-08 15:52:21 +0200

    cms texts

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,6 +3,7 @@ const config = {
   localStorageAuthTokenKey: 'auth-token', // The key for the localstorage entry for the JWT token
   localStorageUserInfoKey: 'user-info', // The key for the localstorage entry for the user info object
   afterLoginRedirectTime: 2000, //The amout of ms after successfull login, after which the user is redirected to the homepage
+  cmsTextsEndpoint: '/cms-texts', // The endpoint to which request for the texts is made on page load
 };
 
 export default config;
