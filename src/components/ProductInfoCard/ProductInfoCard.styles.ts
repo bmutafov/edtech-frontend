ee3b0cd1a73969c694f745556051775dd91898ac
julioandre <julioandre275@gmail.com>
2021-01-10 11:51:34 +0100

    Added Product Card component

---
+++ src/components/ProductInfoCard/ProductInfoCard.styles.ts
@@ -0,0 +1,17 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    flex: '1 0 auto',
+  },
+  cover: {
+    width: 151,
+  },
+});
---
+++ src/components/ProductInfoCard/ProductInfoCard.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+// import { Card, CardMedia, CardContent, Button, Typography, Box } from '@material-ui/core';
+// import { useStyles } from './ProductInfoCard.styles';
+// import { theme } from '../../utils/theme';
+// import { Rating } from '@material-ui/lab';
+
+// const ProductInfoCard: React.FC = () => {
+//   const classes = useStyles();
+//   const [value, setValue] = React.useState<number | null>(4);
+
+//   <Card className={classes.root}>
+//     <CardMedia className={classes.cover} image="emptyimage.jpg" title="Products" />
+//     <div className={classes.details}>
+//       <CardContent>
+//         <Typography component="h5" variant="h5">
+//           Products
+//         </Typography>
+//         <div className={classes.root}>
+//           <Rating name="read-only" value={value} readOnly />
+//           <Box ml={2}>[3.0] Overall Rating</Box>
+//         </div>
+//         <Typography></Typography>
+//       </CardContent>
+//     </div>
+//   </Card>;
+// };
